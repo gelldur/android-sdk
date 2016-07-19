9976f1d696bc055f22be92dea9ed747f555c9fb6
Sasa Sekulic <ja@sasasekulic.info>
2016-07-19 16:28:46 +0200

    added persistence model tests

---
+++ android-sdk/src/androidTest/java/com/sensorberg/sdk/model/persistence/TheBeaconActionShould.java
@@ -0,0 +1,88 @@
+package com.sensorberg.sdk.model.persistence;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import com.sensorberg.sdk.SensorbergTestApplication;
+import com.sensorberg.sdk.action.InAppAction;
+import com.sensorberg.sdk.di.TestComponent;
+import com.sensorberg.sdk.internal.interfaces.Clock;
+import com.sensorberg.sdk.resolver.BeaconEvent;
+import com.sensorberg.sdk.scanner.ScanEventType;
+import com.sensorberg.sdk.testUtils.NoClock;
+
+import org.fest.assertions.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import javax.inject.Inject;
+
+import util.TestConstants;
+
+@RunWith(AndroidJUnit4.class)
+public class TheBeaconActionShould {
+
+    @Inject
+    Gson gson;
+
+    private BeaconAction tested;
+
+    private UUID uuid = UUID.fromString("6133172D-935F-437F-B932-A901265C24B0");
+
+    private Clock clock;
+
+    @Before
+    public void setUp() throws Exception {
+        BeaconEvent beaconEvent = new BeaconEvent.Builder()
+                .withAction(new InAppAction(uuid, null, null, null, null, 0))
+                .withPresentationTime(1337)
+                .withTrigger(ScanEventType.ENTRY.getMask())
+                .build();
+        beaconEvent.setBeaconId(TestConstants.ANY_BEACON_ID);
+
+        ((TestComponent) SensorbergTestApplication.getComponent()).inject(this);
+
+        clock = NoClock.CLOCK;
+        tested = BeaconAction.from(beaconEvent, clock);
+    }
+
+    @Test
+    public void test_tested_object_should_not_be_null() {
+        Assertions.assertThat(tested).isNotNull();
+    }
+
+    @Test
+    public void test_should_be_json_serializeable() throws Exception {
+        String objectAsJSON = gson.toJson(tested);
+        BeaconAction deserializedObject = gson.fromJson(objectAsJSON, BeaconAction.class);
+
+        Assertions.assertThat(objectAsJSON).isNotEmpty();
+        Assertions.assertThat(deserializedObject).isEqualTo(tested);
+    }
+
+    @Test
+    public void test_should_serialize_a_list_of_objects() throws Exception {
+        List<BeaconAction> objects = new ArrayList<>(Arrays.asList(tested));
+        String objectsAsJson = gson.toJson(objects);
+
+        Type listType = new TypeToken<Set<BeaconAction>>() {
+        }.getType();
+        Set<BeaconAction> beaconActions = gson.fromJson(objectsAsJson, listType);
+
+        Assertions.assertThat(beaconActions)
+                .isNotEmpty()
+                .hasSize(1);
+
+        Assertions.assertThat(beaconActions.toArray()[0]).isEqualTo(tested);
+    }
+}
---
+++ android-sdk/src/androidTest/java/com/sensorberg/sdk/model/persistence/TheBeaconScanShould.java
@@ -0,0 +1,77 @@
+package com.sensorberg.sdk.model.persistence;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import com.sensorberg.sdk.SensorbergTestApplication;
+import com.sensorberg.sdk.di.TestComponent;
+import com.sensorberg.sdk.scanner.ScanEvent;
+import com.sensorberg.sdk.scanner.ScanEventType;
+
+import org.fest.assertions.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+import javax.inject.Inject;
+
+import util.TestConstants;
+
+@RunWith(AndroidJUnit4.class)
+public class TheBeaconScanShould {
+
+    @Inject
+    Gson gson;
+
+    private BeaconScan tested;
+
+    @Before
+    public void setUp() throws Exception {
+        ((TestComponent) SensorbergTestApplication.getComponent()).inject(this);
+
+        ScanEvent scanevent = new ScanEvent.Builder()
+                .withEventMask(ScanEventType.ENTRY.getMask())
+                .withBeaconId(TestConstants.ANY_BEACON_ID)
+                .withEventTime(100)
+                .build();
+        tested = BeaconScan.from(scanevent, 0);
+    }
+
+    @Test
+    public void test_should_generate_a_bid() throws Exception {
+        Assertions.assertThat(tested.getPid()).isEqualToIgnoringCase("192E463C9B8E4590A23FD32007299EF50133701337");
+    }
+
+    @Test
+    public void test_should_be_json_serializeable() throws Exception {
+        String objectAsJSON = gson.toJson(tested);
+        BeaconScan deserializedObject = gson.fromJson(objectAsJSON, BeaconScan.class);
+
+        Assertions.assertThat(objectAsJSON).isNotEmpty();
+        Assertions.assertThat(deserializedObject).isEqualTo(tested);
+    }
+
+    @Test
+    public void test_should_serialize_a_list_of_objects() throws Exception {
+        List<BeaconScan> objects = new ArrayList<>(Arrays.asList(tested));
+        String objectsAsJson = gson.toJson(objects);
+
+        Type listType = new TypeToken<Set<BeaconScan>>() {
+        }.getType();
+        Set<BeaconScan> beaconScans = gson.fromJson(objectsAsJson, listType);
+
+        Assertions.assertThat(beaconScans)
+                .isNotEmpty()
+                .hasSize(1);
+
+        Assertions.assertThat(beaconScans.toArray()[0]).isEqualTo(tested);
+    }
+}
