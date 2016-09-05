849051dcfd216401fa6303d50f20478b1a7c1046
Falko Richter <falko@sensorberg.com>
2016-09-05 11:10:54 +0200

    Silent Campaign Tests

--- android-sdk/src/androidTest/java/com/sensorberg/sdk/action/TheActionShould.java
+++ android-sdk/src/androidTest/java/com/sensorberg/sdk/action/TheActionShould.java
@@ -1,46 +1,65 @@
 package com.sensorberg.sdk.action;
 
-import org.fest.assertions.api.Assertions;
-import org.fest.assertions.data.Offset;
-import org.json.JSONException;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
+import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.google.gson.Gson;
+import com.sensorberg.sdk.SensorbergTestApplication;
+import com.sensorberg.sdk.di.TestComponent;
+import com.sensorberg.sdk.model.server.ResolveAction;
+
+import org.fest.assertions.api.Assertions;
+import org.fest.assertions.data.Offset;
+import org.json.JSONException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 import java.io.IOException;
 
+import javax.inject.Inject;
+
 import util.Utils;
+
+import static com.sensorberg.sdk.test.R.raw.action_factory_payload_001_array;
+import static com.sensorberg.sdk.test.R.raw.action_factory_payload_002_object;
+import static com.sensorberg.sdk.test.R.raw.action_factory_payload_003_boolean;
+import static com.sensorberg.sdk.test.R.raw.action_factory_payload_004_integer;
+import static com.sensorberg.sdk.test.R.raw.action_factory_payload_005_string;
+import static com.sensorberg.sdk.test.R.raw.action_factory_payload_006_double;
+import static com.sensorberg.sdk.test.R.raw.action_factory_payload_008_integer_with_exponent;
+import static com.sensorberg.sdk.test.R.raw.action_factory_payload_009_empty_string;
 
 @RunWith(AndroidJUnit4.class)
 public class TheActionShould {
 
-    @Test
-    public void test_not_parse_an_array_as_an_object() throws IOException, JSONException {
-        Action arrayPayloadAction = ActionFactory.actionFromJSONObject(Utils.getRawResourceAsJSON(com.sensorberg.sdk.test.R.raw.action_factory_payload_001_array, InstrumentationRegistry.getContext()));
-        try{
-            arrayPayloadAction.getPayloadJSONObject();
-            Assertions.fail("there was no exception");
-        } catch (JSONException e){
-            //all is fine
-        }
+    private Context context;
+
+    @Inject
+    Gson gson;
+
+    @Before
+    public void setUp() throws Exception {
+        ((TestComponent) SensorbergTestApplication.getComponent()).inject(this);
+        context = InstrumentationRegistry.getContext();
     }
 
-    @Test
+    @Test(expected = JSONException.class)
+    public void test_not_parse_an_array_as_an_object() throws IOException, JSONException {
+        Action arrayPayloadAction = getAction(action_factory_payload_001_array);
+        arrayPayloadAction.getPayloadJSONObject();
+    }
+
+    @Test(expected = JSONException.class)
     public void test_not_parse_an_object_as_an_array() throws IOException, JSONException {
-        Action arrayPayloadAction = ActionFactory.actionFromJSONObject(Utils.getRawResourceAsJSON(com.sensorberg.sdk.test.R.raw.action_factory_payload_002_object, InstrumentationRegistry.getContext()));
-        try{
-            arrayPayloadAction.getPayloadJSONArray();
-            Assertions.fail("there was no exception");
-        } catch (JSONException e){
-            //all is fine
-        }
+        Action arrayPayloadAction = getAction(action_factory_payload_002_object);
+        arrayPayloadAction.getPayloadJSONArray();
     }
 
     @Test
     public void test_allow_parsing_of_booleans() throws IOException, JSONException {
-        Action arrayPayloadAction = ActionFactory.actionFromJSONObject(Utils.getRawResourceAsJSON(com.sensorberg.sdk.test.R.raw.action_factory_payload_003_boolean, InstrumentationRegistry.getContext()));
+        Action arrayPayloadAction = getAction(action_factory_payload_003_boolean);
 
         Boolean output = Boolean.valueOf(arrayPayloadAction.getPayload());
         Assertions.assertThat(output).isEqualTo(true);
@@ -48,7 +67,7 @@ public class TheActionShould {
 
     @Test
     public void test_allow_parsing_of_integer() throws IOException, JSONException {
-        Action arrayPayloadAction = ActionFactory.actionFromJSONObject(Utils.getRawResourceAsJSON(com.sensorberg.sdk.test.R.raw.action_factory_payload_004_integer, InstrumentationRegistry.getContext()));
+        Action arrayPayloadAction = getAction(action_factory_payload_004_integer);
 
         Integer output = Integer.valueOf(arrayPayloadAction.getPayload());
         Assertions.assertThat(output).isEqualTo(1337);
@@ -56,7 +75,7 @@ public class TheActionShould {
 
     @Test
     public void test_allow_parsing_of_strings() throws IOException, JSONException {
-        Action arrayPayloadAction = ActionFactory.actionFromJSONObject(Utils.getRawResourceAsJSON(com.sensorberg.sdk.test.R.raw.action_factory_payload_005_string, InstrumentationRegistry.getContext()));
+        Action arrayPayloadAction = getAction(action_factory_payload_005_string);
 
 
         String output = arrayPayloadAction.getPayload();
@@ -65,7 +84,7 @@ public class TheActionShould {
 
     @Test
     public void test_allow_parsing_of_double_values() throws IOException, JSONException {
-        Action arrayPayloadAction = ActionFactory.actionFromJSONObject(Utils.getRawResourceAsJSON(com.sensorberg.sdk.test.R.raw.action_factory_payload_006_double, InstrumentationRegistry.getContext()));
+        Action arrayPayloadAction = getAction(action_factory_payload_006_double);
 
         Double output = Double.valueOf(arrayPayloadAction.getPayload());
         Assertions.assertThat(output).isEqualTo(1.2345, Offset.offset(0.00001));
@@ -73,7 +92,7 @@ public class TheActionShould {
 
     @Test
     public void test_allow_parsing_of_integerWithExponentValue() throws IOException, JSONException {
-        Action arrayPayloadAction = ActionFactory.actionFromJSONObject(Utils.getRawResourceAsJSON(com.sensorberg.sdk.test.R.raw.action_factory_payload_008_integer_with_exponent, InstrumentationRegistry.getContext()));
+        Action arrayPayloadAction = getAction(action_factory_payload_008_integer_with_exponent);
 
         Double output = Double.valueOf(arrayPayloadAction.getPayload());
         Assertions.assertThat(output).isEqualTo(1337, Offset.offset(0.1));
@@ -81,9 +100,14 @@ public class TheActionShould {
 
     @Test
     public void test_allow_parsing_of_emptyString() throws IOException, JSONException {
-        Action arrayPayloadAction = ActionFactory.actionFromJSONObject(Utils.getRawResourceAsJSON(com.sensorberg.sdk.test.R.raw.action_factory_payload_009_empty_string,
-                InstrumentationRegistry.getContext()));
+        Action arrayPayloadAction = getAction(action_factory_payload_009_empty_string);
 
         Assertions.assertThat(arrayPayloadAction.getPayload()).isNotNull().hasSize(0);
+    }
+
+    private Action getAction(int resourceID) throws IOException {
+        String string = Utils.getRawResourceAsString(resourceID, context);
+        ResolveAction input = gson.fromJson(string, ResolveAction.class);
+        return ResolveAction.BEACON_EVENT_MAPPER.map(input).getAction();
     }
 }
