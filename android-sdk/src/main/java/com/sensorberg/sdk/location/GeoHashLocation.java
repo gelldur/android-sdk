3b4f2a73a2d6e3952086ecd9f67bd15721bec169
Stanislaw Gwara <stanislaw.gwara@sensorberg.com>
2016-11-14 13:36:39 +0100

    Modify Location wrapper

--- android-sdk/src/main/java/com/sensorberg/sdk/location/GeoHashLocation.java
+++ android-sdk/src/main/java/com/sensorberg/sdk/location/GeoHashLocation.java
@@ -2,15 +2,41 @@ package com.sensorberg.sdk.location;
 
 import android.location.Location;
 
+import com.sensorberg.sdk.settings.TimeConstants;
+
 public class GeoHashLocation extends Location {
 
-    private GeoHash geohash;
+    public static final float MIN_ACCURACY_RADIUS = 25.0F;
+    public static final long MAX_LOCATION_AGE = TimeConstants.ONE_MINUTE;
 
-    public GeoHashLocation(String provider) {
-        super(provider);
-    }
+    private String geohash;
+    private boolean valid = false;
 
     public GeoHashLocation(Location location) {
         super(location);
+        if (isAccurateAndFresh()) {
+            geohash = GeoHash.encode(location.getLatitude(), location.getLongitude(), 9).toHashString();
+            valid = true;
+        }
+    }
+
+    public String getValidGeohash() {
+        return isValid() ? geohash : null;
+    }
+
+    public boolean isValid() {
+        if (!valid) {
+            return false;
+        }
+        if (isAccurateAndFresh()) {
+            valid = true;
+        } else {
+            valid = false;
+        }
+        return valid;
+    }
+
+    private boolean isAccurateAndFresh() {
+        return getAccuracy() < MIN_ACCURACY_RADIUS && (System.currentTimeMillis() - getTime()) < MAX_LOCATION_AGE;
     }
 }
