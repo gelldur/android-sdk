8cf4465972fb796d446eff220a6505af63bc0c2e
Ronaldo Pace <ronaldo.pace@sensorberg.com>
2016-11-14 16:06:03 +0100

    RuntimeException, device failed to launch service http://crashes.to/s/7f58622b305

--- android-sdk/src/main/java/com/sensorberg/sdk/receivers/GenericBroadcastReceiver.java
+++ android-sdk/src/main/java/com/sensorberg/sdk/receivers/GenericBroadcastReceiver.java
@@ -1,5 +1,6 @@
 package com.sensorberg.sdk.receivers;
 
+import com.sensorberg.sdk.Logger;
 import com.sensorberg.sdk.SensorbergService;
 
 import android.content.Context;
@@ -16,7 +17,15 @@ public class GenericBroadcastReceiver extends SensorbergBroadcastReceiver{
     public void onReceive(Context context, Intent intent) {
         Intent service = new Intent(context, SensorbergService.class);
         service.putExtras(intent.getExtras());
-        context.startService(service);
+        try {
+            context.startService(service);
+        } catch (RuntimeException e) {
+            // so apparently devices that fail to launch your service.
+            // even if everything is configured correctly
+            // https://chromium.googlesource.com/android_tools/+/master/sdk/sources/android-23/android/app/ContextImpl.java?autodive=0%2F%2F#1256
+            // https://sensorberg.atlassian.net/browse/AND-248
+            Logger.log.logError("System bug throwing error.", e);
+        }
     }
 
     @SuppressWarnings("ConstantConditions")
