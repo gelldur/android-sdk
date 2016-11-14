9894d3eacbfd6617f57e56606c5e8fd59d86287a
Stanislaw Gwara <stanislaw.gwara@sensorberg.com>
2016-11-14 13:30:42 +0100

    Made LocationHelper abstract

--- android-sdk/src/main/java/com/sensorberg/sdk/location/LocationHelper.java
+++ android-sdk/src/main/java/com/sensorberg/sdk/location/LocationHelper.java
@@ -1,91 +1,75 @@
 package com.sensorberg.sdk.location;
 
-import android.Manifest;
-import android.location.Criteria;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Bundle;
-import android.os.Looper;
-import android.os.Message;
-import android.util.Log;
 
-import com.sensorberg.sdk.internal.AndroidHandler;
 import com.sensorberg.sdk.internal.PermissionChecker;
-import com.sensorberg.sdk.internal.interfaces.HandlerManager;
-import com.sensorberg.sdk.internal.interfaces.RunLoop;
 
-import java.util.concurrent.TimeUnit;
-
-import javax.inject.Inject;
-import javax.inject.Named;
-import javax.inject.Singleton;
-
-import hugo.weaving.DebugLog;
 import lombok.Getter;
 
-@Singleton
-public class LocationHelper implements LocationListener, RunLoop.MessageHandlerCallback {
+public abstract class LocationHelper {
 
-    private HandlerManager handlerManager;
-    private LocationManager locationManager;
-    private PermissionChecker permissionChecker;
-    @Getter
-    private Location location;
-    private long minTime = TimeUnit.MINUTES.toMillis(30);
-    private float minDistance = 50.0F;
+    private LocationManager manager;
+    private PermissionChecker checker;
 
-    @Inject @DebugLog
-    public LocationHelper(@Named("realHandlerManager") HandlerManager handlerManager, LocationManager locationManager, PermissionChecker permissionChecker) {
-        Log.d("LocationHelper", "constructor");
-        this.handlerManager = handlerManager;
-        this.locationManager = locationManager;
-        this.permissionChecker = permissionChecker;
-        if (permissionChecker.checkForPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
-            //We've got GPS.
-            Log.d("LocationHelper", "ACCESS_FINE_LOCATION");
-            AndroidHandler handler = (AndroidHandler) handlerManager.getLocationRunLoop(this);
-            Criteria criteria = new Criteria();
-            criteria.setAccuracy(Criteria.ACCURACY_FINE);
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance, this);
-            //locationManager.requestLocationUpdates(minTime, minDistance, criteria, this, handler.getHandler().getLooper());
-        } else if (permissionChecker.checkForPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
-            //We have only coarse location permission
-            Log.d("LocationHelper", "ACCURACY_COARSE");
-            AndroidHandler handler = (AndroidHandler) handlerManager.getLocationRunLoop(this);
-            Criteria criteria = new Criteria();
-            criteria.setAccuracy(Criteria.ACCURACY_COARSE);
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, minTime, minDistance, this);
-            locationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, minTime, minDistance, this);
-            //locationManager.requestLocationUpdates(minTime, minDistance, criteria, this, handler.getHandler().getLooper());
-        } else {
-            //No permission for location.
+    private GeoHashLocation location;
+
+    @Getter private String provider;
+
+    public LocationHelper(LocationManager manager, PermissionChecker checker) {
+        this.manager = manager;
+        this.checker = checker;
+        provider = determineProviderStartup(checker);
+        //TODO listen for provider availability changes (optional)
+        //TODO provider not existing case - list all providers, choose from them.
+        //TODO cache provider/permissions for api >= 23 (?)
+    }
+
+    /**
+     * Get most recent geohash fulfilling accuracy / age boundaries.
+     * This methods works as passively as possible.
+     * @return Geohash if location is within given accuracy and age, null if not.
+     * Also null if the location is not available or permissions are missing.
+     */
+    public String getGeohash() {
+        provider = determineProviderRuntime(checker);
+        if (provider == null) {
+            return null;
         }
+        try {
+            if (manager.isProviderEnabled(provider)) {
+                Location fresh = manager.getLastKnownLocation(provider);
+                location = wrap(fresh);
+                if (location != null) {
+                    return location.getValidGeohash();
+                }
+            }
+        } catch (SecurityException | IllegalArgumentException ex) {
+            //TODO better logging.
+            ex.printStackTrace();
+        }
+        return null;
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
-        this.location = location;
-        Log.d("LocationHelper", location != null ? location.toString() : "null");
-    }
+    /**
+     * Runs in constructor to determine provider at startup.
+     * @param checker PermissionChecker used by abstract super class.
+     * @return Provider string as per LocationManager constants or null if permissions missing.
+     */
+    protected abstract String determineProviderStartup(PermissionChecker checker);
 
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-        Log.d("LocationHelper", s+" "+i+" "+bundle);
-    }
+    /**
+     * Runs on every geohash request to determine provider.
+     * @param checker PermissionChecker used by abstract super class.
+     * @return Provider string as per LocationManager constants or null if permissions missing.
+     */
+    protected abstract String determineProviderRuntime(PermissionChecker checker);
 
-    @Override
-    public void onProviderEnabled(String s) {
-        Log.d("LocationHelper", s);
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-        Log.d("LocationHelper", s);
-    }
-
-    @Override
-    public void handleMessage(Message msg) {
-        Log.d("LocationHelper", msg.getData().toString());
+    private GeoHashLocation wrap(Location fresh) {
+        if (fresh != null && !fresh.equals(location)) {
+            return new GeoHashLocation(fresh);
+        } else {
+            return location;
+        }
     }
 }
