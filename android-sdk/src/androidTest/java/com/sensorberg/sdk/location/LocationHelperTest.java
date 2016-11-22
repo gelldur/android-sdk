14417ac7955b44b0a80c07383d1b87e6edad482b
Stanislaw Gwara <stanislaw.gwara@sensorberg.com>
2016-11-22 14:05:17 +0100

    Updated tests for LocationHelper

--- android-sdk/src/androidTest/java/com/sensorberg/sdk/location/LocationHelperTest.java
+++ android-sdk/src/androidTest/java/com/sensorberg/sdk/location/LocationHelperTest.java
@@ -4,12 +4,19 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.sensorberg.sdk.SensorbergTestApplication;
+import com.sensorberg.sdk.di.TestComponent;
+import com.sensorberg.sdk.settings.SettingsManager;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.Arrays;
 import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
@@ -27,18 +34,24 @@ public class LocationHelperTest {
     private List<String> PROVIDERS;
     private Location l0, l1;
 
+    @Inject @Named("dummyTransportSettingsManager")
+    protected SettingsManager settings;
+
     @Before
     public void setUp() throws Exception {
+
+        ((TestComponent) SensorbergTestApplication.getComponent()).inject(this);
 
         PROVIDERS = Arrays.asList("provider.1", "provider.2");
         l0 = new Location(PROVIDERS.get(0));
         l1 = new Location(PROVIDERS.get(1));
 
         mockedManager = mock(LocationManager.class);
+
         when(mockedManager.getProviders(true)).thenReturn(PROVIDERS);
         when(mockedManager.getLastKnownLocation(PROVIDERS.get(0))).thenReturn(l0);
         when(mockedManager.getLastKnownLocation(PROVIDERS.get(1))).thenReturn(l1);
-        tested = new LocationHelper(mockedManager);
+        tested = new LocationHelper(mockedManager, settings);
     }
 
     @Test
@@ -46,7 +59,7 @@ public class LocationHelperTest {
 
         long now = System.currentTimeMillis();
 
-        l0.setAccuracy(LocationHelper.MIN_ACCURACY_RADIUS - 1);
+        l0.setAccuracy(tested.getGeohashMinAccuracyRadius() - 1);
         l0.setTime(now - 10);
 
         l1.setAccuracy(1);
@@ -63,10 +76,10 @@ public class LocationHelperTest {
     public void location_helper_return_null() throws Exception {
 
         l0.setAccuracy(1);
-        l0.setTime(System.currentTimeMillis() - LocationHelper.MAX_LOCATION_AGE - 10);
+        l0.setTime(System.currentTimeMillis() - tested.getMaxLocationAge() - 10);
 
         l1.setAccuracy(1);
-        l1.setTime(System.currentTimeMillis() - LocationHelper.MAX_LOCATION_AGE - 10);
+        l1.setTime(System.currentTimeMillis() - tested.getMaxLocationAge() - 10);
 
         String hash = tested.getGeohash();
         assertNull(hash);
